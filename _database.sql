287b4bd83cc0868fadca626ee5e3a677e3988916
JoaoV <joaovcarneirog@gmail.com>
2026-05-19 10:24:31 -0300

    insert into em produto, vendedor e clientes

--- _database.sql
+++ _database.sql
@@ -84,3 +84,15 @@ FROM
     vendedor v
 INNER JOIN
     funcionario_especial fe ON v.id = fe.vendedor_id;
+
+INSERT INTO produto (nome, descricao, quantidade_estoque, valor, observacoes, vendedor_id)
+VALUES
+('Mangá One Piece', 'Volume 1', 100, 29.90, 'Traduzido pela editora Panini', 1);
+
+INSERTO INTO vendedor (tipo)
+VALUES
+('Vendedor de Mangás')
+
+INSERTO INTO clientes (nome, idade, sexo, data_nascimento)
+VALUES
+('João Vitor', 21, 'M', '20-05-2004');
