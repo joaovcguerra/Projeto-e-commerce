4e3b60d58159b340273e367edf5daaecd45eae05
Rodrigo Accioly <rodrigomatazombis@gmail.com>
2026-05-24 14:58:52 -0300

    views com inner join ao inves de where

--- Projeto Estruturado/database_views.sql
+++ Projeto Estruturado/database_views.sql
@@ -5,17 +5,19 @@ SELECT
     vendedor.nome,
     COUNT(funcionario_especial.vendedor_id) AS total_bonus,
     SUM(funcionario_especial.bonus) AS valor_bonus
-FROM vendedor, funcionario_especial
-WHERE vendedor.id = funcionario_especial.vendedor_id
+FROM vendedor
+INNER JOIN funcionario_especial
+ON vendedor.id = funcionario_especial.vendedor_id
 GROUP BY vendedor.nome;
 
 CREATE VIEW view_produtos_vendedores AS
-SELECT 
-    vendedor.nome AS vendedor,
-    COUNT(produto.id) AS total_produtos,
-    SUM(produto.valor) AS valor_total_produtos
-FROM produto, vendedor
-WHERE produto.vendedor_id = vendedor.id
+SELECT
+    vendedor.nome,
+    COUNT(produto.id) AS total_produto,
+    SUM(produto.valor) AS total_venda 
+FROM vendedor
+INNER JOIN produto
+ON vendedor.id = produto.vendedor_id
 GROUP BY vendedor.nome;
 
 CREATE VIEW view_vendas_clientes AS
@@ -23,8 +25,9 @@ SELECT
     clientes.nome,
     COUNT(venda.id) AS total_compras,
     SUM(venda.valor_transporte) AS total_transporte
-FROM clientes, venda
-WHERE clientes.id = venda.cliente_id
+FROM clientes 
+INNER JOIN venda
+ON venda.cliente_id = clientes.id
 GROUP BY clientes.nome;
 
 CREATE VIEW view_total_vendas_produto AS
@@ -32,6 +35,7 @@ SELECT
     produto.nome,
     SUM(venda_item.quantidade) AS total_vendido,
     SUM(venda_item.quantidade * venda_item.valor_unitario) AS valor_total
-FROM produto, venda_item
-WHERE produto.id = venda_item.produto_id
+FROM produto
+INNER JOIN venda_item
+ON produto.id = venda_item.produto_id
 GROUP BY produto.nome;
